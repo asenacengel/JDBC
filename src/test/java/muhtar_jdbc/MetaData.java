02d6c6060ebd013a02c4fcdad9bc79b43026f51b
Asena Cengel <asena.eroglu@gmail.com>
2020-05-16 15:12:18 -0400

    first commit

--- src/test/java/muhtar_jdbc/MetaData.java
+++ src/test/java/muhtar_jdbc/MetaData.java
@@ -2,6 +2,8 @@ package muhtar_jdbc;
 
 import Utility.DBUtility;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class MetaData {
@@ -10,5 +12,16 @@ public class MetaData {
         System.out.println(DBUtility.metaData.getUserName());
         System.out.println(DBUtility.metaData.getDriverVersion());
         System.out.println(DBUtility.metaData.getDatabaseProductName());
+
+        ResultSet rs = DBUtility.getResult("select * from employees");
+        ResultSetMetaData resultData = rs.getMetaData();
+        System.out.println(resultData.getColumnCount());
+        System.out.println(resultData.getColumnName(5));
+        System.out.println("===============================");
+
+        for (int columnCount = resultData.getColumnCount(); columnCount > 1; columnCount++) {
+            System.out.println(resultData.getColumnName(columnCount));
+        }
+
     }
 }
